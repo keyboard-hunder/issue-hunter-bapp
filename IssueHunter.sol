8b385a33284615861d6fc5639bf4ac2f53795d2f
Jongwoo Na <njw1204@naver.com>
2019-06-27 03:05:23 +0900

    Add solving func to contract

--- IssueHunter.sol
+++ IssueHunter.sol
@@ -37,20 +37,41 @@ contract IssueHunter is Ownable {
         erc20 = new_erc20;
     }
 
-    function makeIssue(string repo, string title, string[] tags, uint256 price) public {
-        require(msg.sender.balance >= price);
+    function makeIssue(string repo, string title, string[] tags, uint256 price) public payable {
+        require(msg.value == price);
+
         Issue memory issue = Issue(
             issues.length, msg.sender, repo, title, tags, price, false, true
         );
         issues.push(issue);
-        issueBy[msg.sender].push(issues.length - 1);
+        issueBy[msg.sender].push(issues.length.sub(1));
     }
 
-    function editIssue(uint256 _id, string repo, string title, string[] tags, uint256 price, bool active) public {
+    function editIssueContents(uint256 _id, string repo, string title, string[] tags, bool active) public {
         require(issues[_id].owner == msg.sender && issues[_id].solved == false);
+
         Issue memory issue = Issue(
-            _id, msg.sender, repo, title, tags, price, issues[_id].solved, active
+            _id, msg.sender, repo, title, tags, issues[_id].price, issues[_id].solved, active
         );
         issues[_id] = issue;
+    }
+
+    function editIssuePrice(uint256 _id, uint256 price) public payable {
+        require(msg.value == price);
+        require(issues[_id].owner == msg.sender && issues[_id].solved == false);
+
+        msg.sender.transfer(issues[_id].price);
+        issues[_id].price = price;
+    }
+
+    function _markSolvedIssue(uint256 _id, address by) internal onlyOwner {
+        require(issues[_id].owner != address(0) && issues[_id].solved == false);
+        issues[_id].solved = true;
+        solvedIssueOf[by].push(_id);
+    }
+
+    function solve(uint256 _id, address by) public onlyOwner {
+        _markSolvedIssue(_id, by);
+        erc20.transfer(by, issues[_id].price);
     }
 }
